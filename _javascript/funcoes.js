77eab06b78d11227fae8e9df822e805f3393b510
joaopaulopires <joaopaulopiresdev@gmail.com>
2020-05-16 08:52:00 -0300

    alteração java e background-color

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -2,3 +2,8 @@ function mudaFoto (foto)
 {
     document.getElementById("icone").src = foto
 }
+function calc_total () {
+    var qtd = parseInt(document.getElementeById('cQtd').value);
+    tot = qtd * 1500;
+    document.getElementById('cTot').value = tot;
+}
